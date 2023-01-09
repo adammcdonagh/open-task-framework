6a814adca01e58c10a61a8f1906246f1116c1f9b
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-01-09 13:36:11 +0000

    Tidy up to make automated tests possible

--- src/opentaskpy/remotehandlers/scripts/transfer.py
+++ src/opentaskpy/remotehandlers/scripts/transfer.py
@@ -4,6 +4,7 @@ import os
 import re
 import pwd
 import grp
+import shutil
 
 # Remote script intended to serve as a utility to the main script for dealing with file transfers
 
@@ -32,7 +33,68 @@ def list_files(pattern, details=False):
     return files
 
 
+def delete_files(files, delimiter):
+    files = files.split(delimiter)
+    for file in list(files):
+        print(f"Deleting {file}")
+        os.unlink(file)
+
+
+def move_files(files, delimiter, destination, create_dest_dir, owner, group, mode, rename_regex, rename_sub):
+    # Split the moveFiles arg into a list
+    files = files.split(delimiter)
+    for file in list(files):
+
+        file = os.path.expanduser(file)
+        print(f"Handling {file}")
+
+        # Change ownership and permissions
+        if owner:
+            uid = pwd.getpwnam(owner).pw_uid
+            print(f"Setting owner to {owner}")
+            os.chown(file, uid, -1)
+
+        if group:
+            gid = grp.getgrnam(group).gr_gid
+            print(f"Setting group to {group}")
+            os.chown(file, -1, gid)
+
+        if mode:
+            print(f"Setting mode to {mode}")
+            os.chmod(file, int(mode, 8))
+
+        # Apply any regex substitution if needed
+        if rename_regex:
+
+            orig_filename = os.path.basename(file)
+            orig_dirname = os.path.dirname(file)
+            new_filename = f"{orig_dirname}/{re.sub(rename_regex, rename_sub, orig_filename)}"
+            print(f"Renaming: {file} to {new_filename}")
+            os.rename(file, new_filename)
+            file = new_filename
+
+        # Verify the destination directory exists
+        if not os.path.exists(destination) and create_dest_dir:
+            print(f"Creating destination directory: {destination}")
+            os.makedirs(destination)
+        elif not os.path.exists(destination):
+            print(f"ERROR: Destination directory does not exist: {destination}, and not requested to create it")
+            raise FileNotFoundError
+
+        # Now we can move the file into it's final location
+        filename = os.path.basename(file)
+        print(f"Moving {file} to {destination}/{filename}")
+
+        # Save the existing permissions on the file
+        file_stat = os.stat(file)
+        shutil.move(file, f"{destination}/{filename}")
+        # Move doesn't preserve ownership, so we need to do that manually
+        os.chown(f"{destination}/{filename}", file_stat.st_uid, file_stat.st_gid)
+        os.chmod(f"{destination}/{filename}", file_stat.st_mode)
+
+
 def main():
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--listFiles", help="Regex of files to look for", type=str, required=False)
     parser.add_argument("-d", "--details", help="Include file details in output", action="store_true", required=False)
@@ -46,6 +108,19 @@ def main():
     parser.add_argument("--mode", help="Mode to set on files to be moved", required=False)
     parser.add_argument("--renameRegex", help="Regex expression to apply to filenames", required=False)
     parser.add_argument("--renameSub", help="Regex substitution to apply to filenames", required=False)
+    parser.add_argument(
+        "--delimiter",
+        help="Character(s) used to separate filenames",
+        required=False,
+        type=str,
+        default="|||",
+    )
+    parser.add_argument(
+        "--createDestDir",
+        help="Whether to create the destination directory, if it doesn't exist",
+        required=False,
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
@@ -53,48 +128,20 @@ def main():
         print(json.dumps(list_files(args.listFiles, args.details)))
 
     if args.deleteFiles:
-        files = args.deleteFiles.split()
-        for file in list(files):
-            print(f"Deleting {file}")
-            os.unlink(file)
+        delete_files(args.deleteFiles, args.delimiter)
 
     if args.moveFiles:
-        # Split the moveFiles arg into a list
-        files = args.moveFiles.split()
-        for file in list(files):
-
-            file = os.path.expanduser(file)
-            print(f"Handling {file}")
-
-            # Change ownership and permissions
-            if args.owner:
-                uid = pwd.getpwnam(args.owner).pw_uid
-                print(f"Setting owner to {args.owner}")
-                os.chown(file, uid, -1)
-
-            if args.group:
-                gid = grp.getgrnam(args.group).gr_gid
-                print(f"Setting group to {args.group}")
-                os.chown(file, -1, gid)
-
-            if args.mode:
-                print(f"Setting mode to {args.mode}")
-                os.chmod(file, int(args.mode, 8))
-
-            # Apply any regex substitution if needed
-            if args.renameRegex:
-
-                orig_filename = os.path.basename(file)
-                orig_dirname = os.path.dirname(file)
-                new_filename = f"{orig_dirname}/{re.sub(args.renameRegex, args.renameSub, orig_filename)}"
-                print(f"Renaming: {file} to {new_filename}")
-                os.rename(file, new_filename)
-                file = new_filename
-
-            # Now we can move the file into it's final location
-            filename = os.path.basename(file)
-            print(f"Moving {file} to {args.destination}/{filename}")
-            os.rename(file, f"{args.destination}/{filename}")
+        move_files(
+            args.moveFiles,
+            args.delimiter,
+            args.destination,
+            args.createDestDir,
+            args.owner,
+            args.group,
+            args.mode,
+            args.renameRegex,
+            args.renameSub,
+        )
 
 
 if __name__ == "__main__":
