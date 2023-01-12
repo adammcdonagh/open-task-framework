3300f10756f62e8ecac3465f3cfbab29127500ec
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-01-12 14:25:54 +0000

    Alter JSON variable lookup to use JSONPath

--- src/opentaskpy/plugins/lookup/http_json.py
+++ src/opentaskpy/plugins/lookup/http_json.py
@@ -1,13 +1,14 @@
 """
 Basic HTTP JSON lookup plugin
 
-Does an HTTP GET request to the given URL and returns the value of an attribuet JSON response.
+Does an HTTP GET request to the given URL and returns the value of an attribute JSON response.
 This does not support any type of authentication, and just expects a simple
-JSON response from which to extract a value.
+JSON response from which to extract a value using a provided JSONPath.
 
 """
 import logging
 import requests
+from jsonpath_ng import parse
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +18,7 @@ plugin_name = "http_json"
 def run(**kwargs):
 
     # Expect a kwarg named url, and value
-    expected_kwargs = ["url", "value"]
+    expected_kwargs = ["url", "jsonpath"]
     for kwarg in expected_kwargs:
         if kwarg not in kwargs:
             raise Exception(f"Missing kwarg: '{kwarg}' while trying to run lookup plugin '{plugin_name}'")
@@ -26,7 +27,10 @@ def run(**kwargs):
     try:
         response = requests.get(kwargs["url"])
         response.raise_for_status()
-        result = response.json()[kwargs["value"]]
+        json = response.json()
+        jsonpath_expr = parse(kwargs["jsonpath"])
+        result = [match.value for match in jsonpath_expr.find(json)][0]
+
         logger.log(12, f"Read '{result}' from URL {kwargs['url']}")
     except Exception as e:
         logger.error(f"Failed to read from URL {kwargs['url']}: {e}")
---
+++ src/tests/test_plugin_http_json.py
@@ -0,0 +1,32 @@
+import unittest
+from opentaskpy.plugins.lookup.http_json import run
+
+
+class HttpJsonPluginTest(unittest.TestCase):
+    def test_http_json_plugin_missing_all(self):
+        with self.assertRaises(Exception) as ex:
+            run()
+
+        self.assertIn("Missing kwarg:", str(ex.exception))
+
+    def test_http_json_plugin_missing_url(self):
+        with self.assertRaises(Exception) as ex:
+            run(jsonpath="test")
+
+        self.assertEqual(str(ex.exception), "Missing kwarg: 'url' while trying to run lookup plugin 'http_json'")
+
+    def test_http_json_plugin_missing_jsonpath(self):
+        with self.assertRaises(Exception) as ex:
+            run(url="http://test.com")
+
+        self.assertEqual(str(ex.exception), "Missing kwarg: 'jsonpath' while trying to run lookup plugin 'http_json'")
+
+    def test_http_json_plugin(self):
+        # Run test with a valid URL and JSONPath
+        result = run(url="https://jsonplaceholder.typicode.com/todos/1", jsonpath="$.userId")
+        self.assertEqual(result, 1)
+
+    def test_http_json_plugin_complex(self):
+        # A more complex JSONPath
+        result = run(url="https://jsonplaceholder.typicode.com/todos", jsonpath="$[0].title")
+        self.assertEqual(result, "delectus aut autem")
