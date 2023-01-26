82a22e5dbcdae360ad6e7c977cfb4d1440a3a538
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-01-26 16:14:51 +0000

    Remove -it

--- test/setupSSHKeys.sh
+++ test/setupSSHKeys.sh
@@ -19,15 +19,15 @@ cp $DIR/testFiles/id_rsa ~/.ssh/
 chown -R $USER ~/.ssh
 chmod -R 700 ~/.ssh
 
-docker exec -it ssh_1 /bin/sh -c "usermod -G operator -a application"
-docker exec -it ssh_2 /bin/sh -c "usermod -G operator -a application"
-docker exec -it ssh_1 /bin/sh -c "mkdir -p ~application/.ssh"
-docker exec -it ssh_2 /bin/sh -c "mkdir -p ~application/.ssh"
+docker exec ssh_1 /bin/sh -c "usermod -G operator -a application"
+docker exec ssh_2 /bin/sh -c "usermod -G operator -a application"
+docker exec ssh_1 /bin/sh -c "mkdir -p ~application/.ssh"
+docker exec ssh_2 /bin/sh -c "mkdir -p ~application/.ssh"
 
 docker cp $DIR/testFiles/id_rsa ssh_1:/home/application/.ssh 
 docker cp $DIR/testFiles/id_rsa.pub ssh_1:/home/application/.ssh/authorized_keys
-docker exec -it ssh_1 /bin/sh -c "chown -R application ~application/.ssh && chmod -R 700 ~application/.ssh"
+docker exec ssh_1 /bin/sh -c "chown -R application ~application/.ssh && chmod -R 700 ~application/.ssh"
 
 docker cp $DIR/testFiles/id_rsa ssh_2:/home/application/.ssh 
 docker cp $DIR/testFiles/id_rsa.pub ssh_2:/home/application/.ssh/authorized_keys
-docker exec -it ssh_2 /bin/sh -c "chown -R application ~application/.ssh && chmod -R 700 ~application/.ssh"
+docker exec ssh_2 /bin/sh -c "chown -R application ~application/.ssh && chmod -R 700 ~application/.ssh"
