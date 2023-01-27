e8ec2a78ef3ea29f03e88425c836a07a8dcfbe8b
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-01-27 17:02:45 +0000

    Start at refactoring for proper pypi packaging

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-"""Setup function for opentaskpy package."""
-from setuptools import setup
-
-setup(
-    name="open-task-framework",
-    install_requires=["jsonschema", "paramiko", "jinja2"],
-    packages=["opentaskpy"],
-    scripts=["bin/task-run"],
-)
