412e04190164852757030bfcc4875d6915c4f634
Adam McDonagh <adam@elitemonkey.net>
2025-09-20 12:32:10 +0000

    Tidy GPG logic and move gpg home to a temp dir

--- src/opentaskpy/taskhandlers/transfer.py
+++ src/opentaskpy/taskhandlers/transfer.py
@@ -46,17 +46,27 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
 
         # Set up the local staging directory
         # Allow for a custom staging directory to be set via environment variable
-        staging_dir_name = f"OTF_STAGING_{getpid()}.{random.randint(0, 1000000)}"
-        if "OTF_STAGING_DIR" in environ:
-            self.local_staging_dir = f"{environ['OTF_STAGING_DIR']}/{staging_dir_name}"
-        else:
-            self.local_staging_dir = f"{DEFAULT_STAGING_DIR_BASE}/{staging_dir_name}"
+        self.local_staging_dir = self._get_staging_directory()
 
         self.logger = opentaskpy.otflogging.init_logging(
             "opentaskpy.taskhandlers.transfer", self.task_id, TASK_TYPE
         )
 
         super().__init__(global_config)
+
+    def _get_staging_directory(self) -> str:
+        """GEt a unique staging directory path.
+
+        Uses the OTF_STAGING_DIR environment variable if set, otherwise uses the default base.
+
+        Returns:
+            str: The full path to the staging directory
+        """
+        staging_dir_name = f"OTF_STAGING_{getpid()}.{random.randint(0, 1000000)}"
+        if "OTF_STAGING_DIR" in environ:
+            return f"{environ['OTF_STAGING_DIR']}/{staging_dir_name}"
+
+        return f"{DEFAULT_STAGING_DIR_BASE}/{staging_dir_name}"
 
     def return_result(
         self,
@@ -655,6 +665,29 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
 
         return self.return_result(0)
 
+    def _setup_gnupg(self) -> gnupg.GPG:
+        """Setup gnupg object with homedir set to a temporary directory.
+
+        Returns:
+            gnupg.GPG: The gnupg object
+        """
+        # Make the gnupg home directory in a temporary directory, use the
+        # original directory, and create it if it doesn't exist
+        gnupg_home_dir = f"{self._get_staging_directory()}/.gnupg"
+        makedirs(gnupg_home_dir, exist_ok=True)
+
+        return gnupg.GPG(gnupghome=f"{gnupg_home_dir}/.gnupg")
+
+    def _tidy_gnupg_home(self, gpg: gnupg.GPG) -> None:
+        """Tidy up the gnupg homedir."""
+        if path.exists(gpg.gnupghome):
+            try:
+                shutil.rmtree(gpg.gnupghome, ignore_errors=True)
+            except FileNotFoundError as e:
+                self.logger.warning(
+                    f".gnupg deletion failed - FileNotFound but continuing: {e}"
+                )
+
     def encrypt_files(
         self,
         files: dict,
@@ -673,15 +706,8 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
         Returns:
             dict: Dictionary of encrypted files.
         """
-        # Use the local staging dir (or the path where the files live) as the gnupg home
-        # Get the dirname of the first file in the list
-        tmpdir = path.dirname(list(files.keys())[0])
-
-        # Make the gnupg home directory
-        makedirs(f"{tmpdir}/.gnupg", exist_ok=True)
-
         # Set up gnupg
-        gpg = gnupg.GPG(gnupghome=f"{tmpdir}/.gnupg")
+        gpg = self._setup_gnupg()
 
         # Remove any escaped newline characters from the key
         public_key = public_key.replace("\\n", "\n")
@@ -738,14 +764,8 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
 
             encrypted_files[output_filename] = files[file]
 
-        # Remove the temporary gnupg keychain files under f"{tmpdir}/.gnupg"
-        if path.exists(f"{tmpdir}/.gnupg"):
-            try:
-                shutil.rmtree(f"{tmpdir}/.gnupg", ignore_errors=True)
-            except FileNotFoundError as e:
-                self.logger.warning(
-                    f".gnupg deletion failed - FileNotFound but continuing: {e}"
-                )
+        # Remove the temporary gnupg keychain files under gpg.gnupghome
+        self._tidy_gnupg_home(gpg)
 
         return encrypted_files
 
@@ -759,15 +779,11 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
         Returns:
             dict: Dictionary of decrypted files.
         """
-        # Use the local staging dir (or the path where the files live) as the gnupg home
         # Get the dirname of the first file in the list
         tmpdir = path.dirname(list(files.keys())[0])
 
-        # Make the gnupg home directory
-        makedirs(f"{tmpdir}/.gnupg", exist_ok=True)
-
         # Set up gnupg
-        gpg = gnupg.GPG(gnupghome=f"{tmpdir}/.gnupg")
+        gpg = self._setup_gnupg()
 
         private_key = private_key.replace("\\n", "\n")
 
@@ -820,15 +836,8 @@ class Transfer(TaskHandler):  # pylint: disable=too-many-instance-attributes
 
             decrypted_files[output_filename] = files[file]
 
-        # Remove the temporary gnupg keychain files under f"{tmpdir}/.gnupg"
-        # Check if the directory exists first
-        if path.exists(f"{tmpdir}/.gnupg"):
-            try:
-                shutil.rmtree(f"{tmpdir}/.gnupg", ignore_errors=True)
-            except FileNotFoundError as e:
-                self.logger.warning(
-                    f".gnupg deletion failed - FileNotFound but continuing: {e}"
-                )
+        # Tidy up the gnupg homedir
+        self._tidy_gnupg_home(gpg)
 
         self.logger.debug(f"Returning decrypted files: {decrypted_files}")
 
